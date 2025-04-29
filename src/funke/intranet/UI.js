25388ad695e38b3ae491afd6bfd8e4ce4b4e3e68
hartmutboekhoff <hartmut.boekhoff@t-online.de>
2025-04-29 09:20:08 +0200

    UI for Funke Intranet

---
+++ src/funke/intranet/UI.js
@@ -0,0 +1,25 @@
+(function(){
+  window.addEventListener('load',()=>{
+    
+    
+    // ================================================
+    console.log('initializing shortcut-keys');
+    //window.addKeyHandler('Space',ev=>alert('key pressed'));
+  
+  
+    // ================================================
+    console.log('initializing read-out elements');
+    GM_sessionStorage.setItem('language', 'de-DE');
+    //window.registerForReadOut('selector');
+    window.registerForReadOut('ol[data-searchpanelcontenttype="body"] li', {language: 'de-DE'});
+
+    // ================================================
+    console.log('initializing mutation-reactions');
+    //window.onMutation('selector', reaction);
+  
+  
+    // ================================================
+
+
+  });
+})();
---
+++ src/funke/intranet/main.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Funke Intranet
+// @description  UI Verbesserungen für Funke Intranet
+// @version      1
+// @grant        GM_getResourceURL
+// @grant        GM_getResourceText
+// @grant        GM_info
+// @grant        addStyle
+// @grant        unsafeWindow
+
+// @include      https://funkemedien.sharepoint.com/*
+
+// @downloadURL          http://localhost:3000/funke/intranet/main.js
+// @updateURL            http://localhost:3000/funke/intranet/main.js
+
+// @resource  css-common http://localhost:3000/common/styles.css
+// @resource  css        http://localhost:3000/funke/intranet/styles.css
+
+// @require         http://localhost:3000/common/head.js
+// @require         http://localhost:3000/common/utility.js
+// @require         http://localhost:3000/common/styles.js
+// @require         http://localhost:3000/common/KeyHandler.js
+// @require         http://localhost:3000/common/UnifiedSessionStorage.js
+// @require         http://localhost:3000/common/readout.js
+
+// @require         http://localhost:3000/funke/intranet/UI.js
+
+// ==/UserScript==
+
+console.log(GM_info.script.name, 'Version '+GM_info.script.version, 'Initialization complete');
+console.groupEnd();
