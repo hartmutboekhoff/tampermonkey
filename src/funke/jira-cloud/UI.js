1896d3a674ef5ac486e77b15ca2a72d79fe096c0
hartmutboekhoff <hartmut.boekhoff@t-online.de>
2025-06-11 17:03:02 +0200

    read comments on comments

--- src/funke/jira-cloud/UI.js
+++ src/funke/jira-cloud/UI.js
@@ -11,7 +11,7 @@
     console.log('initializing read-out elements');
     //window.registerForReadOut('selector');
     window.registerForReadOut('div[data-editor-container-id="issue-description-editor"]');
-    window.registerForReadOut('div[data-testid="issue.activity.comments-list"] > div > span > div > div:nth-child(2)', {
+    window.registerForReadOut('div[data-testid="issue.activity.comments-list"] > div > span > div > div:nth-child(n+2)', {
       exclude: ['button'],
       childElements: {
         'h3': {
